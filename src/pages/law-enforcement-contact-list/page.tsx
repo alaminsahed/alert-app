8f4561ab9a89052d096ca34d718c93dbab50ddf1
Sakil Khan <sakilk130@gmail.com>
2024-08-08 06:06:05 +0600

    feat: list page

--- src/pages/law-enforcement-contact-list/page.tsx
+++ src/pages/law-enforcement-contact-list/page.tsx
@@ -1,14 +1,83 @@
-import { Card } from 'antd';
+import { CopyOutlined, PhoneOutlined } from '@ant-design/icons';
+import { Card, Collapse, message } from 'antd';
+import { divisions } from 'utils/numbers';
 
 const LawEnforcementContactListPage = () => {
+  const copyToClipboard = (text: any) => {
+    navigator.clipboard
+      .writeText(text)
+      .then(() => {
+        message.success('Phone number copied to clipboard!');
+      })
+      .catch((err) => {
+        message.error('Failed to copy');
+      });
+  };
+
   return (
     <Card
       title={'Law Enforcement Contact List'}
       bodyStyle={{
-        padding: '34px ',
+        padding: '34px 90px',
       }}
     >
-      Test
+      {divisions.map((div, i) => (
+        <Collapse
+          items={[
+            {
+              key: i,
+              label: div.name,
+              children: div.areas.map((a, i) => (
+                <Collapse
+                  items={[
+                    {
+                      key: i + 1,
+                      label: a.name,
+                      children: a.contacts.map((c) => (
+                        <div
+                          style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'space-between',
+                          }}
+                        >
+                          <div>{c.number}</div>
+                          <div>
+                            <PhoneOutlined
+                              style={{
+                                height: 30,
+                                width: 30,
+                                cursor: 'pointer',
+                              }}
+                              onClick={() => {
+                                window.location.href = `tel:${c.raw}`;
+                              }}
+                            />
+                            <CopyOutlined
+                              style={{
+                                height: 30,
+                                width: 30,
+                                cursor: 'pointer',
+                              }}
+                              onClick={() => copyToClipboard(c.number)}
+                            />
+                          </div>
+                        </div>
+                      )),
+                    },
+                  ]}
+                  style={{
+                    marginBottom: '10px',
+                  }}
+                />
+              )),
+            },
+          ]}
+          style={{
+            marginBottom: '10px',
+          }}
+        />
+      ))}
     </Card>
   );
 };
